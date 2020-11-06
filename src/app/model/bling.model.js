289db6596cd4115c342510194a7e680c7a03f952
1garo <alevardai@hotmail.com>
2020-11-06 13:06:06 -0300

    feat: handle some error

--- src/app/model/bling.model.js
+++ src/app/model/bling.model.js
@@ -1,16 +1,18 @@
 const ConnectBling = require('../../services/connectBling.service');
 const ObjectToXml = require('../utils/bling/objectToXml');
+const Bling = require('./Bling');
 
 class BlingModel {
   static async insertOrdered(data) {
     const arrayXml = await new ObjectToXml(data).xmlModel();
     const resultArray = [];
-
     arrayXml.forEach((element) => resultArray.push(new ConnectBling('/pedido/json/').post({
       xml: element,
+    }).catch(error => {
+      return Promise.reject(error)
     })));
     return Promise.all(resultArray).catch((error) => {
-      throw new Error(error);
+      Promise.reject(error);
     });
   }
 }
