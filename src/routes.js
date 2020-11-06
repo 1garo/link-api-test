17ba3a9fccf2d1bc5d94e76116cd9c00ee43799e
1garo <alevardai@hotmail.com>
2020-11-06 13:44:14 -0300

    feat: add new route to query database

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,6 @@ const blingController = require("./app/controller/bling.controller");
 routes.get("/bling", blingController.index);
 
 routes.post("/bling", blingController.store);
-//TODO: Implement pipe calls
-// curl -X GET "https://bling.com.br/Api/v2/pedidos/json/"
-// -G
-// -d "apikey=a42cd0e54d8c1d5c8b0d5be23fb52e80371f74222273e532aaa4f26b2f04100c56fda7ef"
-// routes.get("/pipe", pipeController.index);
-// curl -X POST "https://bling.com.br/Api/v2/pedido/json/"
-     //-d "apikey={apikey}"
-     //-d "xml={xml_do_pedido}"https://manuais.bling.com.br/manual/?item=situacoes
-// routes.post("/pipe", pipeController.store);
+
+routes.get("/query_db", blingController.query_db);
 module.exports = routes;
